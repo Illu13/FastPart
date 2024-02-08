a5cf90a6c2182ec7ba1bba9ccb0992a3845f26c5
Isaac Delgado <113700281+Illu13@users.noreply.github.com>
2024-02-08 20:29:32 +0100

    feat(views):
    Added user-gestion views.
    
    login.blade.php: this view has been personalized in order to change colors and to add a footer,

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,36 @@
+<footer class="grid grid-cols-3  gap-20 footer mt-5 ">
+    <div class="md:col-span-3">
+        <p class="lead">
+            <!-- Link to the Terms and Conditions page. -->
+            <a href="/view/termsAndConditions.php">Términos y condiciones</a>
+        </p>
+        <p class="lead">
+            <!-- Placeholder link to Legal Notice. -->
+            <a href="/view/termsAndConditions.php#legalWarn">Aviso legal</a>
+        </p>
+        <p class="lead">
+            <!-- Placeholder link to Privacy Policy. -->
+            <a href="/view/termsAndConditions.php#privPol">Política de privacidad</a>
+        </p>
+    </div>
+    <div class="md:col-span-3">
+        <a href="/index.php">
+            <x-application-logo class="w-20 h-20 fill-current text-gray-500"/>
+        </a>
+    </div>
+    <div class="md:col-span-3">
+        <p class="lead">
+            <!-- Link to Google Maps with the restaurant address. -->
+            <a href="https://www.google.com/maps/place/C.+Juego+de+Pelota,+14900+Lucena,+C%C3%B3rdoba/data=!4m2!3m1!1s0xd6d7734a3a1046f:0x37967444a4027296?sa=X&ved=2ahUKEwi116fU1saCAxVT1AIHHSxoAiIQ8gF6BAg9EAA">C
+                / Juego de Pelota</a>
+        </p>
+        <p class="lead">
+            <!-- Placeholder text for order information. -->
+            Nº 0 Datos
+        </p>
+        <p class="lead">
+            <!-- Contact phone number. -->
+            +34 333333333
+        </p>
+    </div>
+</footer>
